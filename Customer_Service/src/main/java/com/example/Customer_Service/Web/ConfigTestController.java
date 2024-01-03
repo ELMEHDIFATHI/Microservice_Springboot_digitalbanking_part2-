4c365016c40568f2da606be8ddc01e72f506b78a
ELMEHDIFATHI <mehdiblabo@gmail.com>
2024-01-03 17:43:57 +0100

    2

--- Customer_Service/src/main/java/com/example/Customer_Service/Web/ConfigTestController.java
+++ Customer_Service/src/main/java/com/example/Customer_Service/Web/ConfigTestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RefreshScope
+
 public class ConfigTestController {
     @Value("${global.params.p1}")
     private int p1;
